2a3a2eb8e3ea4ccb7ec4638818b1ac6647eb9a95
nithinreddy392200-cmd <nithinreddy392200@gmail.com>
2026-02-19 21:13:31 +0530

    Add files via upload

---
+++ upper_triangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+    int main()
+  {
+    int rows;
+    int col;
+    printf("enter no of rows:\n");
+    scanf("%d",&rows);
+    printf("enter no of colums:\n");
+    scanf("%d",&col);
+    for(int i=1;i<=col;i++) 
+    {
+    for(int j=1;j<=col;j++)
+    {
+    if(i==1||j==col||i==j||i<j)
+    {
+       printf("* ");
+    } 
+    else
+    {
+    printf("  ");
+   }
+  }
+   printf ("\n");
+   }
+  }
