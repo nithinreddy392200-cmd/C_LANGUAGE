d60809b09dfb017735aa04b333bb3a332c97379a
nithinreddy392200-cmd <nithinreddy392200@gmail.com>
2026-03-29 22:58:45 +0530

    Add files via upload

---
+++ branding.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void printBrandLogo() {
+    printf("=====================================\n");
+    printf("   *****   BRAND-X INNOVATIONS   *****\n");
+    printf("=====================================\n");
+}
+
+void printTagline() {
+    printf("Tagline: \"Turning Ideas Into Impact\"\n");
+}
+
+int main() {
+    printBrandLogo();
+    printTagline();
+    return 0;
+}
